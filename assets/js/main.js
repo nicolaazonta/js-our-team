8858131d37f3bc123e7b191dcc19821d0c361a7c
nicolaazonta <nicolazonta114@gmail.com>
2023-03-22 15:59:07 +0100

    complete all milestones with bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ Organizzare i singoli membri in card/schede
 
 //MILESTONE 0:
 //Creare l’array di oggetti con le informazioni fornite.
-const profilesArray =[
+const profilesArray = [
     {//------------------------------------------------0
         name: 'Wayne Barnett',
         mansion: 'Founder & CEO',
@@ -59,11 +59,41 @@ const profilesArray =[
 
 //MILESTONE 1:
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-for (let i = 0; i < profilesArray.length; i++) {
+/* for (let i = 0; i < profilesArray.length; i++) {
    for(let key in profilesArray[i]){
     let thisObject = profilesArray[i];
     console.log(thisObject.name,thisObject.mansion,thisObject.pic );   
    } 
+} */
+
+/* MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede */
+for (let i = 0; i < profilesArray.length; i++) {
+
+    let markup = ` 
+    <div class="col-4 my-2">   
+        <div class="card bg-dark p-2 "><!-- /.card -->
+            <img class="card-img" src="/assets/img/${profilesArray[i].pic}" alt="">
+
+            <div class="card-img-overlay d-flex ">
+                <h3 class="card-title align-self-end">${profilesArray[i].name}</h3>
+                <h8 class="card-subtitle align-self-end">${profilesArray[i].mansion}</h8>
+            </div>            
+        </div>
+    </div>`;
+
+    document.querySelector('.row').innerHTML += markup;
+
+
+
+    /* for(let key in profilesArray[i]){
+     let markup = ``
+     
+        let thisObject = profilesArray[i];
+     console.log(thisObject.name,thisObject.mansion,thisObject.pic );   
+    }  */
 }
-
-
