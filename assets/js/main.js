388d501fc1e2e01bc83d779f89d38ba230bfc2b0
nicolaazonta <nicolazonta114@gmail.com>
2023-03-22 15:01:22 +0100

    complete milestone 0

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.  Ogni membro è caratterizzato dalle seguenti informazioni:
+nome
+ruolo
+foto
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede
+ */
+
+
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+const profilesArray =[
+    {//------------------------------------------------0
+        name: 'Wayne Barnett',
+        mansion: 'Founder & CEO',
+        pic: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {//------------------------------------------------1
+        name: 'Chief Editor',
+        mansion: 'Founder & CEO',
+        pic: 'angela-caroll-chief-editor.jpg'
+    },
+
+    {//------------------------------------------------2
+        name: 'Walter Gordon',
+        mansion: 'Office Manager',
+        pic: 'walter-gordon-office-manager.jpg'
+    },
+
+    {//------------------------------------------------3
+        name: 'Angela Lopez',
+        mansion: 'Social Media Manager',
+        pic: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {//------------------------------------------------4
+        name: 'Scott Estrada',
+        mansion: 'Developer',
+        pic: 'scott-estrada-developer.jpg'
+    },
+
+    {//------------------------------------------------5
+        name: 'Barbara Ramos',
+        mansion: 'Graphic Designer',
+        pic: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+//console.log(profilesArray[0].name);
+
+
